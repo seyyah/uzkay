87143c70ddf529ca56d5eb780855ab2f349c48e9
Recai Oktaş (uzak) <roktas@debian.org>
2010-08-03 00:52:18 +0300

    bitti gibi

--- inc/kaydet.php
+++ inc/kaydet.php
@@ -1,30 +1,42 @@
 <?php
 
 F3::clear('message');
-//echo F3::get('SESSION.captcha');
 F3::call(':denetle');
 
-function yukle($yuklenen, $yeni) {
-	if (file_is_uploaded($yuklenen) {
-		$filesize = filesize($yuklenen);
-		$mimetype = exif_imagetype($yuklenen);
+function yukle($hedef=NULL) {
+	$yuklenen = F3::get('FILES.file.tmp_name');
 
-		if ($filesize > 350000) {
+	// hedef ve yüklenen dosyanın boş olmasına izin veriyoruz
+	// herhangi biri boşsa mesele yok, çağırana dön
+	if (empty($hedef) || empty($yuklenen)) {
+		return true;
+	}
+
+	// bu bir uploaded dosya olmalı, fake dosyalara izin yok
+	if (is_uploaded_file($yuklenen)) {
+		// boyutu sınırla, değeri öylesine seçtim
+		if (filesize($yuklenen) > 350000) {
 			F3::set('message', 'Resim çok büyük');
 		}
-		else if ($mimetype != IMAGETYPE_JPEG) {
+		// şimdilik sadece JPEG, dosya tipini içine bakarak tespit ediyoruz
+		else if (exif_imagetype($yuklenen) != IMAGETYPE_JPEG) {
 			F3::set('message', 'Resim JPEG değil');
 		}
-		else if (! move_uploaded_file($yuklenen, $yeni)) {
+		// dosyanın üzerine yazmayalım, ekstra güvenlik
+		else if (file_exists($hedef)) {
+			F3::set('message', 'Resim zaten kaydedilmiş');
+		}
+		// tamamdır, kalıcı kayıt yapalım
+		else if (!move_uploaded_file($yuklenen, $hedef)) {
 			F3::set('message', 'Dosya yükleme hatası');
 		}
 		else {
 			return true;
-			; // ok
 		}
 	}
 	else {
-		F3::set('message', 'Dosya yüklenmemiş');
+		// bu aslında bir atak işareti
+		F3::set('message', 'Dosya geçerli bir yükleme değil');
 	}
 
 	return false;
@@ -35,17 +47,21 @@ if (! F3::exists('message')) {
 	$kul->copyFrom('REQUEST');
 	$kul->tarih = date("h:i / d-m-Y");
 
-	$yuklenen = $_FILES['file']['tmp_name'];
-	if (! empty($yuklenen)) {
-		// TODO: tc numara ile kayıt yapacağız, $kul->tc
-		$yeni = "./resim/foo.jpg";
-		yukle($yuklenen, $yeni); // dönüş değeri yerine message kontrolü yapacağım
-	}
+	$resim = 'resim/' . $kul->tc . '.jpg';
+
+	// here we go!
+	yukle($resim);
 
 	if (! F3::exists('message')) {
 		$kul->save();
 		// TODO: burada bir özet verelim
 		echo "Başarıyla kaydedildi.";
+	}
+	// hatalı bir resim kaydı varsa çöp bırakmamaya çalış
+	// FIXME: bu mantık üzerinde biraz daha çalış
+	else if (file_exists($yeni) && ! unlink($yeni)) {
+		// TODO ne yazayım ben şimdi buraya
+		;
 	}
 }
 else {
