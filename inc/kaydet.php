2a4d53ae07f410e6b2d6dbad0557a0d11230d154
Recai Oktaş <roktas@bil.omu.edu.tr>
2010-08-03 00:15:59 +0300

    resim yükleme için ilk deneme

--- inc/kaydet.php
+++ inc/kaydet.php
@@ -4,14 +4,49 @@ F3::clear('message');
 //echo F3::get('SESSION.captcha');
 F3::call(':denetle');
 
+function yukle($yuklenen, $yeni) {
+	if (file_is_uploaded($yuklenen) {
+		$filesize = filesize($yuklenen);
+		$mimetype = exif_imagetype($yuklenen);
+
+		if ($filesize > 350000) {
+			F3::set('message', 'Resim çok büyük');
+		}
+		else if ($mimetype != IMAGETYPE_JPEG) {
+			F3::set('message', 'Resim JPEG değil');
+		}
+		else if (! move_uploaded_file($yuklenen, $yeni)) {
+			F3::set('message', 'Dosya yükleme hatası');
+		}
+		else {
+			return true;
+			; // ok
+		}
+	}
+	else {
+		F3::set('message', 'Dosya yüklenmemiş');
+	}
+
+	return false;
+}
+
 if (! F3::exists('message')) {
 	$kul = new Axon('kul');
 	$kul->copyFrom('REQUEST');
 	$kul->tarih = date("h:i / d-m-Y");
 
-	$kul->save();
-	// TODO: bir özet ver
-	echo "Başarıyla kaydedildi.";
+	$yuklenen = $_FILES['file']['tmp_name'];
+	if (! empty($yuklenen)) {
+		// TODO: tc numara ile kayıt yapacağız, $kul->tc
+		$yeni = "./resim/foo.jpg";
+		yukle($yuklenen, $yeni); // dönüş değeri yerine message kontrolü yapacağım
+	}
+
+	if (! F3::exists('message')) {
+		$kul->save();
+		// TODO: burada bir özet verelim
+		echo "Başarıyla kaydedildi.";
+	}
 }
 else {
 	// Hata var, dön başa ve tekrar kayıt al.
