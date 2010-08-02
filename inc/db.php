1f449588d4d83345f6f4e86dc8025ab4a5be46ad
Recai Oktaş (uzak) <roktas@debian.org>
2010-08-02 18:26:38 +0300

    modeli ayıralım, doğru pratik bu

---
+++ inc/db.php
@@ -0,0 +1,30 @@
+<?php
+
+function is_table_exists($table, $db=NULL) {
+	if (is_null($db))
+		$db = F3::get('DB.name');
+	return $db && F3::sql(
+		array(
+			"SELECT COUNT(*) AS found ".
+			"FROM information_schema.tables ".
+			"WHERE table_schema='$db' ".
+			"AND table_name='$table';"
+		)
+	);
+}
+
+if (is_table_exists('kul')) {
+	// FIXME init.sql'deki tabloyu buraya aktar
+	F3::sql(
+		array(
+			'CREATE TABLE kul ('.
+				'tc INT (11) UNSIGNED NOT NULL,'.
+				'ad CHAR (15),'.
+				'soyad CHAR (20),'.
+				'PRIMARY KEY(tc)'.
+			');' 
+		)
+	);
+}
+
+?>
