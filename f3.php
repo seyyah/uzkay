5b7a4498d7b045443816ee900a2214b72791f9ef
Recai Oktaş <roktas@bil.omu.edu.tr>
2010-08-02 14:18:47 +0300

    kozmetik

--- f3.php
+++ f3.php
@@ -7,8 +7,8 @@ function render() { echo F3::serve('layout.htm'); }
 F3::config(".f3.ini");
 
 F3::route("GET $F3/captcha", ':captcha');
-F3::route("POST $F3/kaydet", ':kaydet');
 F3::route("GET $F3/", ':al');
+F3::route("POST $F3/kaydet", ':kaydet');
 
 F3::run();
 
