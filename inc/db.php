468dea0e035fa3e9ab1004687b71463ebf9e748c
Recai Oktaş <roktas@bil.omu.edu.tr>
2010-08-03 02:49:29 +0300

    bitirmeye doğru

--- inc/db.php
+++ inc/db.php
@@ -17,14 +17,29 @@ function is_table_exists($table, $db=NULL) {
 	);
 }
 
-// FIXME init.sql'deki tabloyu buraya aktar
 F3::sql(
 	array(
 		'CREATE TABLE IF NOT EXISTS kul ('.
 			'id INT(11) NOT NULL auto_increment,'.
 			'tc VARCHAR(11) NOT NULL,'.
-			'ad CHAR (15),'.
-			'soyad CHAR (20),'.
+			'ad CHAR (32),'.
+			'soyad CHAR (32),'.
+			'kizliksoyad varchar(32),'.
+			'babaad varchar(32),'.
+			'anaad varchar(32),'.
+			'ceptel varchar(11),'.
+			'evtel varchar(11),'.
+			'email varchar(100),'.
+			'evadres varchar(100),'.
+			'il varchar(32),'.
+			'ilce varchar(32),'.
+			'uni varchar(100),'.
+			'bolum varchar(100),'.
+			'calismakurum varchar(100),'.
+			'calismabirim varchar(100),'.
+			'isadres varchar(100),'.
+			'isil varchar(32),'.
+			'onkayit varchar(1),'.
 			'tarih varchar(100),'.
 			'PRIMARY KEY(id)'.
 		');' 
